556f7871bc23526f3e2c8172c4c6c0688bcbb264
murilojcavalcanti <josemurilo7@gmail.com>
2024-08-29 18:00:09 -0300

    Atualização de arquitetura

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<ApplicationBuilder> Opts):base(Opts) {}
+
+    }
+}
